19cf64c304854f034e99da91ea831e0db67a311f
Danny Maziad <dmaziad@gmail.com>
2019-11-23 12:26:17 -0500

    Implement Hooks in App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,36 @@
-import React from "react";
-import { render } from "react-dom";
-import { handleData, getData } from "../lib/fetch.js";
-import Options from "./Options";
-import Schedule from "./Schedule";
+import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+import { getData } from '../lib/fetch.js';
+import Options from './Options';
+import Schedule from './Schedule';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      view: "byDate",
-      dates: [],
-      rounds: []
-    };
+const App = () => {
+  let [view, setView] = useState('byDate');
+  let [dates, setDates] = useState([]);
+  let [rounds, setRounds] = useState([]);
 
-    this.toggleView = this.toggleView.bind(this);
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     getData().then(data => {
-      this.setState({ ...data });
+      setDates(data.dates);
+      setRounds(data.rounds);
     });
-  }
+  });
 
-  toggleView(view) {
-    this.setState({ view: view });
-  }
+  const toggleView = view => {
+    setView(view);
+  };
 
-  render() {
-    const { view, dates, rounds } = this.state;
-    return (
-      <div className="app">
-        <div className="heading-bar">
-          <h2 className="title">
-            <span className="heading">Schedule</span>
-          </h2>
-        </div>
-        <Options toggleView={this.toggleView} view={view} />
-        <Schedule dates={dates} view={view} rounds={rounds} />
+  return (
+    <div className="app">
+      <div className="heading-bar">
+        <h2 className="title">
+          <span className="heading">Schedule</span>
+        </h2>
       </div>
-    );
-  }
-}
+      <Options toggleView={toggleView} view={view} />
+      <Schedule dates={dates} view={view} rounds={rounds} />
+    </div>
+  );
+};
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
